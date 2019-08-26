17ffc8d118f8462dee4f06267f8a9a4ecf7db261
Terry <terryvilver@gmail.com>
2019-08-26 13:55:30 +0200

    ajout de utilisation UtilisationDoWhile

---
+++ exo_java/UtilisationDoWhile.java
@@ -0,0 +1,39 @@
+//Ce programme a pour but de demander le prénom de l'utilisateur
+
+import java.util.Scanner;
+
+public class UtilisationDoWhile{
+
+    static void question(char lettre){
+        Scanner scan = new Scanner(System.in);
+        String prenom;
+
+        do
+         {
+            System.out.print("Comment t'appelles-tu ? ");
+            prenom = scan.nextLine();
+            System.out.println( "Bonjour " + prenom +" !");
+
+            do
+            {
+                System.out.print("\n");
+                System.out.print("Veux tu me présenter quelqu'un d'autre ou te représenter ? ('o' ou 'n')\nVotre réponse:");
+                lettre = scan.nextLine().charAt(0);
+            }while( (lettre != 'o') && (lettre != 'n') );
+         }while(lettre =='o');
+
+        System.out.print("Au revoir !");
+
+    }
+
+
+    public static void main(String []args){
+        char lettre=' '; /*on initialise notre charactère a la valeur voulu, c'est 
+                            à dire 'o', pour rentrer dans la boucle "while" à notre
+                            fonction suivante */
+
+        question(lettre);
+
+    }
+
+}
