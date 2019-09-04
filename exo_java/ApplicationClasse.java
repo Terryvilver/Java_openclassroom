3dff36b7ac86f2c09d1cebf6bd68e00b6b9384c7
Terry <terryvilver@gmail.com>
2019-09-04 21:03:44 +0200

    Utilisation de la covariance des variabes

--- exo_java/ApplicationClasse.java
+++ exo_java/ApplicationClasse.java
@@ -1,13 +1,46 @@
 //Mise en application du cours :
 //https://openclassrooms.com/en/courses/26832-apprenez-a-programmer-en-java/21282-creez-votre-premiere-classe
-
 import java.util.Scanner;
+/********************* CLASSE VOITURE ***********************/
+class Voiture extends Vehicule{
+    protected String option;
+    /************** CONSTRUCTEUR DERIVEE *****************/
+    public Voiture(){
+        super(); //appel le constructeur de la classe mère
+        option = " ";
+    }
+    public Voiture(String pMarque, String pModele, int pAnnee, int pCv, String pOption){
+        super(pMarque, pModele, pAnnee, pCv); //appel le constructeur avec parametre de la classe mère
+        option = pOption;
+    }
+    /********** ACCESSEUR ***************/
+    public String getOption()
+    {
+        return option;
+    }
 
+    /********************** MUTATEUR *****************/
+    public void SetOption(String pOption)
+    {
+        option = pOption;
+    }
+
+    /************ METHODE D'INSTANCE DERIVEE *****************/
+    public String toString()
+    {
+        return super.toString() + " En plus elle possède une " + this.option+".";
+    }
+
+}
+
+/************************ CLASSE VEHICULE ******************/
 class Vehicule{
-    private String marque;
-    private String modele;
-    private int annee; //année d'immatriculation
-    private static int nbreVehicule = 0;
+    protected String marque;
+    protected String modele;
+    protected int annee; //année d'immatriculation
+    protected static int nbreVehicule = 0;
+    protected int cv = 0;
+    protected char categorie;
 
     /*********************** Constructeur ********************************/
     public Vehicule() //Constructeur sans paramètre
@@ -15,15 +48,19 @@ class Vehicule{
         String marque = " ";
         String modele = " ";
         int annee = 0;
+        int cv = 0;
+        this.setCategorie();
         System.out.println("création d'un véhicule sans paramètre.");
         nbreVehicule++;
     }
 
-    public Vehicule(String pMarque, String pModele, int pAnnee)
+    public Vehicule(String pMarque, String pModele, int pAnnee, int pCv)
     {
         marque = pMarque;
         modele = pModele;
         annee = pAnnee;
+        cv = pCv;
+        this.setCategorie();
         System.out.println("création de véhicule avec paramètre.");
         nbreVehicule++;
     }
@@ -42,6 +79,17 @@ class Vehicule{
     {
         return annee;
     }
+
+    public int getCv()
+    {
+        return cv;
+    }
+
+    public char getCategorie()
+    {
+        return categorie;
+    }
+
 
     public static int getnbreVehicule()
     {
@@ -64,6 +112,25 @@ class Vehicule{
         annee = pAnnee;
     }
 
+    public void setCv(int pCv)
+    {
+        cv = pCv;
+        this.setCategorie();
+    }
+
+    private void setCategorie()
+    {
+        char tab_categorie[] = {'?','A','B','C','D','S'};
+        int tab_cv[] = {0, 15, 30, 60, 100, 400};
+
+        int i = 0;
+
+        while( (i < tab_cv.length) && (this.cv >= tab_cv[i]) )
+            i++;
+
+        this.categorie = tab_categorie[i];
+    }
+
     public String comparer(Vehicule v) //Compare les dates d'immatriculation des 2 Véhicules.
     {
         if(this.annee < v.getAnnee())
@@ -74,11 +141,10 @@ class Vehicule{
             return "Le véhicule de marque " + v.getMarque() + " est plus ancien que le véhicule de marque " + this.marque + ".";
     }
 
-    public String description()
+    public String toString()
     {
-        return "\n\tCe vehicule est de marque " + this.marque + " et de modèle " + this.modele + ", il a été immatriculé en " + this.annee +".";
+        return "\n Ce vehicule est de marque " + this.marque + ". \n De modèle " + this.modele + ". \n Il a été immatriculé en " + this.annee +". \n Possède "+this.cv+" chevaux et est de catégorie " + this.getCategorie()+".";
     }
-
 }
 
 public class ApplicationClasse{
@@ -87,15 +153,33 @@ public class ApplicationClasse{
         Scanner scan = new Scanner(System.in);
 
         //déclaration et initialisation des objets véhicule.
-        Vehicule v1 = new Vehicule("Peugeot","208", 2019); 
-        Vehicule v2 = new Vehicule("Dacia","Lodgy", 2019); 
+        Vehicule tab[] = new Vehicule[4];
 
-        System.out.println(v1.description());
-        System.out.println(v2.description());
-        System.out.print("\n" + v1.comparer(v2) + "\n\n");
-        System.out.println("Il y a " + v1.getnbreVehicule() + " véhicules dans votre garage.");
+        String marqueTab[] = {"Peugeot", "Renault","Tesla","mitsubishi"};
+        int cvTab[] = {15, 80, 200, 115};
+        int anneeTab[] = { 2015, 2016, 2019, 2018};
 
+        for(int i = 0; i < tab.length ; i++)
+        {
+            if(i < 2)
+            {
+                System.out.println("test"+i);
+                Vehicule v = new Vehicule(marqueTab[i],"Peu importe",anneeTab[i], cvTab[i]);
+                tab[i] = v;
+                System.out.println("test"+i);
+            }
+            else
+            {
+                Voiture  vo = new Voiture(marqueTab[i],"Peu importe",anneeTab[i], cvTab[i],"option");
+                tab[i] = vo;
+                System.out.println("test"+i);
+            }
+        }
 
+        
+        for(Object obj : tab) //Covariance des variables appel de la bonne méthode toString en fonction de l'object Voiture ou Véhicule utilisé.
+        {
+            System.out.println(obj);
+        }
     }
-
 }
